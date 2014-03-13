3518550a7834e02bd22a8ce00f75b9b6c02cca01
Jorge Alberto Lozano Charry <jolberto_63@hotmail.com>
2014-03-13 16:46:20 -0500

    Prueba commit

--- src/persistence/Node.java
+++ src/persistence/Node.java
@@ -1,5 +1,39 @@
 package persistence;
 
 public class Node {
+	
+	private short storage;
+	private short memory;
+	private short processing;
+	
+	public Node(short storage, short memory, short processing){
+		this.storage=storage;
+		this.memory=memory;
+		this.processing=processing;
+	}
+
+	public short getStorage() {
+		return storage;
+	}
+
+	public void setStorage(short storage) {
+		this.storage = storage;
+	}
+
+	public short getMemory() {
+		return memory;
+	}
+
+	public void setMemory(short memory) {
+		this.memory = memory;
+	}
+
+	public short getProcessing() {
+		return processing;
+	}
+
+	public void setProcessing(short processing) {
+		this.processing = processing;
+	}
 
 }
