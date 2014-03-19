054c0a340b8b599dcbca70ea7b573896ad00bbd9
jorge cuervo <georgea2107@hotmail.com>
2014-03-19 17:27:02 -0500

    GUINodeParalelo

---
+++ src/presentation/GUINodeParalelo.java
@@ -0,0 +1,16 @@
+package presentation;
+
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+
+public class GUINodeParalelo extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public GUINodeParalelo() {
+		setLayout(null);
+
+	}
+
+}
